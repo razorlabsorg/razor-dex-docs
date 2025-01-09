a27e3b27a09b9d7986b972412fd8cda40fd27dee
Nate <89227631+0xNeit@users.noreply.github.com>
2025-01-09 22:46:19 +0100

    Update Razor DEX documentation configuration and landing page layout
    - Enhanced `vocs.config.tsx` with updated title, base URL, and social links.
    - Improved sidebar structure and added versioning and changelog links.
    - Revamped `pages/index.mdx` to feature a new layout, including a detailed description of Razor Kit and its features.
    - Added community engagement links and improved button navigation for better user experience.

---
+++ sidebar.ts
@@ -0,0 +1,7 @@
+import { Sidebar } from "vocs";
+
+export const sidebar: Sidebar = {
+  '/docs/': [
+    
+  ]
+}
--- vocs.config.tsx
+++ vocs.config.tsx
@@ -1,15 +1,85 @@
 import { defineConfig } from 'vocs'
+import { sidebar } from './sidebar'
+import pkg from './package.json'
 
 export default defineConfig({
-  title: 'Docs',
-  sidebar: [
+  title: 'Razor DEX Docs',
+  baseUrl:
+    process.env.NODE_ENV === 'production'
+      ? 'https://docs.razordex.xyz'
+      : 'http://localhost:5173',
+  titleTemplate: '%s . Razor DEX',
+  description: 'Documentation for Razor DEX, the pioneer DEX on Movement network',
+  editLink: {
+    pattern:
+      'https://github.com/razorlabsorg/razor-dex-docs/edit/master/pages/:path',
+    text: 'Suggest changes to this page',
+  },
+  iconUrl: { light: '/favicon.png', dark: '/favicon.png' },
+  logoUrl: { light: '/logo.png', dark: '/logo.png' },
+  rootDir: '.',
+  search: {
+    boostDocument(documentId) {
+      if (documentId.startsWith('pages/docs')) return 3
+      return 1
+    },
+  },
+  sidebar,
+  socials: [
     {
-      text: 'Getting Started',
-      link: '/getting-started',
+      icon: 'github',
+      link: 'https://github.com/razorlabsorg',
     },
     {
-      text: 'Example',
-      link: '/example',
+      icon: 'discord',
+      link: 'https://discord.gg/razordao',
+    },
+    {
+      icon: 'x',
+      link: 'https://x.com/razordex_',
+    },
+  ],
+  theme: {
+    accentColor: {
+      light: '#ff9318',
+      dark: '#ffc517',
+    },
+  },
+  topNav: [
+    { text: 'Docs', link: '/docs/getting-started', match: '/docs' },
+    {
+      text: 'SDKs',
+      link: '/docs/sdks',
+    },
+    {
+      text: 'Contracts',
+      link: '/docs/contracts'
+    },
+    {
+      text: 'APIs',
+      link: '/docs/apis'
+    },
+    {
+      text: `v${pkg.version}`,
+      items: [
+        {
+          text: `Migrating to ${toPatchVersionRange(pkg.version)}`,
+          link: `/docs/migration-guide#_${toPatchVersionRange(pkg.version).replace(/\./g, '-')}-breaking-changes`,
+        },
+        {
+          text: 'Changelog',
+          link: 'https://github.com/razorlabsorg/razorkit/blob/master/CHANGELOG.md',
+        },
+        {
+          text: 'Contributing',
+          link: 'https://github.com/razorlabsorg/razorkit/blob/master/.github/CONTRIBUTING.md',
+        },
+      ],
     },
   ],
 })
+
+function toPatchVersionRange(version: string) {
+  const [major, minor] = version.split('.').slice(0, 2)
+  return `${major}.${minor}.x`
+}
